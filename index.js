10b9a05abb1f80321c535ed710de1d3a71af6172
Sean Yu <abraxasyu@gmail.com>
2017-10-20 13:19:57 -0500

    testing changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('', function (req, res) {
-  res.send('Hello World!');
+  res.send('Minimal demo application');
 });
 app.listen(33333, function () {
   console.log('Example app listening on port 33333!');
